5d260bcc68ef41a00e6f9be72d0f16af2a8810a2
choi2864 <85035853+choi2864@users.noreply.github.com>
2021-12-01 16:58:32 +0900

    Update MemberVo.java

--- src/prj/trip/member/vo/MemberVo.java
+++ src/prj/trip/member/vo/MemberVo.java
@@ -1,71 +1,90 @@
 package memebr.vo;
 
-package memebr.vo;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-public class MemberVo {
-	private String uid ;
-	private String upw ;
-	private String uname ;
-	private String uemail ;
-	private String birth ;
-	private String tel ;
-	private String gender ;
-	public MemberVo(String uid, String upw, String uname, String uemail, String birth, String tel, String gender) {
-		super();
-		this.uid = uid;
-		this.upw = upw;
-		this.uname = uname;
-		this.uemail = uemail;
-		this.birth = birth;
-		this.tel = tel;
-		this.gender = gender;
-	}
-	public String getUid() {
-		return uid;
-	}
-	public void setUid(String uid) {
-		this.uid = uid;
-	}
-	public String getUpw() {
-		return upw;
-	}
-	public void setUpw(String upw) {
-		this.upw = upw;
-	}
-	public String getUname() {
-		return uname;
-	}
-	public void setUname(String uname) {
-		this.uname = uname;
-	}
-	public String getUemail() {
-		return uemail;
-	}
-	public void setUemail(String uemail) {
-		this.uemail = uemail;
-	}
-	public String getBirth() {
-		return birth;
-	}
-	public void setBirth(String birth) {
-		this.birth = birth;
-	}
-	public String getTel() {
-		return tel;
-	}
-	public void setTel(String tel) {
-		this.tel = tel;
-	}
-	public String getGender() {
-		return gender;
-	}
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-	@Override
-	public String toString() {
-		return "MemberVo [uid=" + uid + ", upw=" + upw + ", uname=" + uname + ", uemail=" + uemail + ", birth=" + birth
-				+ ", tel=" + tel + ", gender=" + gender + "]";
+import db.DBConn;
+import memebr.vo.MemberVo;
+
+public class MemberDao {
+	
+	private Connection conn;
+	private PreparedStatement pstmt;
+	
+	public void InsertUser(MemberVo vo) {
+		String gender ="";
+		if(vo.getGender().equals("f")) {
+			gender = "여";
+		}else if(vo.getGender().equals("m")) {
+			gender = "남";
+		}
+		try {
+			System.out.println("진입 성공");
+			DBConn db = new DBConn();
+			conn = db.getConnection();
+			
+			String sql ="INSERT INTO MEM  VALUES ( (SELECT NVL( MAX(MEM_num),0 )+1 FROM MEM) ,0,?,?,?,?,?,?,?,?,?) ";
+			pstmt = conn.prepareStatement(sql);
+			
+			pstmt.setString(1, vo.getUname());
+			pstmt.setString(2, vo.getUid());
+			pstmt.setString(3, vo.getUpw());
+			pstmt.setString(4, gender);
+			pstmt.setString(5, vo.getAddr());
+			pstmt.setString(6, vo.getTel());
+			pstmt.setString(7, vo.getBirth());
+			pstmt.setString(8, vo.getNick());
+			pstmt.setString(9, vo.getUemail());
+			
+			pstmt.executeUpdate();
+			
+			conn.commit();
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(pstmt != null )  pstmt.close();
+				if(conn  != null )  conn.close();
+			} catch (SQLException e) {
+			}
+		}
 	}
 	
+public int IdCheck(String id) {
+		
+		ResultSet      rs         = null;
+		
+		try {
+			DBConn    db   =  new DBConn();
+			conn           =  db.getConnection();
+			String    sql  =  " SELECT  MEM_num";
+					  sql += " FROM   MEM ";
+					  sql += " WHERE  MEM_num =  ? ";
+			pstmt          =  conn.prepareStatement(sql);
+			
+			pstmt.setString(1, id);
+			
+			rs       =  pstmt.executeQuery();
+			if( rs.next() ) {
+				return  0 ;
+				}else {
+				return  1 ;
+				}
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				if(rs    != null )  rs.close();
+				if(pstmt != null )  pstmt.close();
+				//if(conn  != null )  conn.close();
+			} catch (SQLException e) {
+			}
+		}
+
+		return   -1;
+	}
 }
