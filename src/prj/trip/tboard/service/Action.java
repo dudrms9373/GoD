4471528d940a14ad0f8a68173479d5144275aedd
choi2864 <85035853+choi2864@users.noreply.github.com>
2021-12-01 16:59:49 +0900

    Update Action.java

--- src/prj/trip/tboard/service/Action.java
+++ src/prj/trip/tboard/service/Action.java
@@ -8,10 +8,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.json.simple.JSONObject;
 
-
-
+@FunctionalInterface
 public interface Action {
 
 	public void excute(HttpServletRequest request,
