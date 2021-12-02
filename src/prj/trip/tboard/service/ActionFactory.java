a71cd7ca41436e2a05f671ac80b7ce8259736fa6
choi2864 <85035853+choi2864@users.noreply.github.com>
2021-12-02 15:41:55 +0900

    Update ActionFactory.java

--- src/prj/trip/tboard/service/ActionFactory.java
+++ src/prj/trip/tboard/service/ActionFactory.java
@@ -2,6 +2,8 @@ package prj.trip.tboard.service;
 
 import prj.trip.tboard.impl.IdCheck;
 import prj.trip.tboard.impl.InsertUser;
+import TBoard.impl.Login;
+import TBoard.impl.LoginOut;
 
 public class ActionFactory {
 
@@ -15,6 +17,13 @@ public class ActionFactory {
 		case "IdCheck":
 			action = new IdCheck();
 			break;
+		case "Login":
+			action = new Login();
+			break;
+		case "LoginOut":
+			action = new LoginOut();
+			break;
+				
 		default: break;
 		}
 		
