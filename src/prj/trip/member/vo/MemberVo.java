f2d24ba901d4a93730835233a8f2e39119cf018a
zerogeun <55429152+dudrms9373@users.noreply.github.com>
2021-12-02 10:53:09 +0900

    Add files via upload

--- src/prj/trip/member/vo/MemberVo.java
+++ src/prj/trip/member/vo/MemberVo.java
@@ -1,90 +1,159 @@
-package memebr.vo;
+package prj.trip.member.vo;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 
-import db.DBConn;
-import memebr.vo.MemberVo;
-
-public class MemberDao {
+public class MemberVo{
 	
-	private Connection conn;
-	private PreparedStatement pstmt;
+	//Fields
+	int mem_num; // 고유번호
+	int mem_level; // 900 이상 매니저 임명
+	String mem_name;
+	String mem_id;
+	String mem_pwd;
+	String mem_gender;
+	String mem_addr;
+	String mem_tel;
+	String mem_birth;
+	String mem_nick; // 닉네임
+	String mem_email;
 	
-	public void InsertUser(MemberVo vo) {
-		String gender ="";
-		if(vo.getGender().equals("f")) {
-			gender = "여";
-		}else if(vo.getGender().equals("m")) {
-			gender = "남";
-		}
-		try {
-			System.out.println("진입 성공");
-			DBConn db = new DBConn();
-			conn = db.getConnection();
-			
-			String sql ="INSERT INTO MEM  VALUES ( (SELECT NVL( MAX(MEM_num),0 )+1 FROM MEM) ,0,?,?,?,?,?,?,?,?,?) ";
-			pstmt = conn.prepareStatement(sql);
-			
-			pstmt.setString(1, vo.getUname());
-			pstmt.setString(2, vo.getUid());
-			pstmt.setString(3, vo.getUpw());
-			pstmt.setString(4, gender);
-			pstmt.setString(5, vo.getAddr());
-			pstmt.setString(6, vo.getTel());
-			pstmt.setString(7, vo.getBirth());
-			pstmt.setString(8, vo.getNick());
-			pstmt.setString(9, vo.getUemail());
-			
-			pstmt.executeUpdate();
-			
-			conn.commit();
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if(pstmt != null )  pstmt.close();
-				if(conn  != null )  conn.close();
-			} catch (SQLException e) {
-			}
-		}
+	
+	//Constructor
+	public MemberVo(){}
+
+	public MemberVo(int mem_num, int mem_level, String mem_name, String mem_id, String mem_pwd, String mem_gender,
+			String mem_addr, String mem_tel, String mem_birth, String mem_nick, String mem_email) {
+		
+		this.mem_num = mem_num;
+		this.mem_level = mem_level;
+		this.mem_name = mem_name;
+		this.mem_id = mem_id;
+		this.mem_pwd = mem_pwd;
+		this.mem_gender = mem_gender;
+		this.mem_addr = mem_addr;
+		this.mem_tel = mem_tel;
+		this.mem_birth = mem_birth;
+		this.mem_nick = mem_nick;
+		this.mem_email = mem_email;
+	}
+
+	
+	//InsertUser Vo
+	public MemberVo(String mem_id, String mem_pwd, String mem_name, String mem_email, String mem_nick, String mem_addr, String mem_birth,
+			String mem_tel, String mem_gender) {
+
+		this.mem_name = mem_name;
+		this.mem_id = mem_id;
+		this.mem_pwd = mem_pwd;
+		this.mem_gender = mem_gender;
+		this.mem_addr = mem_addr;
+		this.mem_tel = mem_tel;
+		this.mem_birth = mem_birth;
+		this.mem_nick = mem_nick;
+		this.mem_email = mem_email;
+	
+	}
+
+	
+
+
+	// Getter/Setter
+	public int getMem_num() {
+		return mem_num;
+	}
+
+	public void setMem_num(int mem_num) {
+		this.mem_num = mem_num;
+	}
+
+	public int getMem_level() {
+		return mem_level;
+	}
+
+	public void setMem_level(int mem_level) {
+		this.mem_level = mem_level;
+	}
+
+	public String getMem_name() {
+		return mem_name;
+	}
+
+	public void setMem_name(String mem_name) {
+		this.mem_name = mem_name;
+	}
+
+	public String getMem_id() {
+		return mem_id;
+	}
+
+	public void setMem_id(String mem_id) {
+		this.mem_id = mem_id;
+	}
+
+	public String getMem_pwd() {
+		return mem_pwd;
+	}
+
+	public void setMem_pwd(String mem_pwd) {
+		this.mem_pwd = mem_pwd;
+	}
+
+	public String getMem_gender() {
+		return mem_gender;
+	}
+
+	public void setMem_gender(String mem_gender) {
+		this.mem_gender = mem_gender;
+	}
+
+	public String getMem_addr() {
+		return mem_addr;
+	}
+
+	public void setMem_addr(String mem_addr) {
+		this.mem_addr = mem_addr;
+	}
+
+	public String getMem_tel() {
+		return mem_tel;
+	}
+
+	public void setMem_tel(String mem_tel) {
+		this.mem_tel = mem_tel;
+	}
+
+	public String getMem_birth() {
+		return mem_birth;
+	}
+
+	public void setMem_birth(String mem_birth) {
+		this.mem_birth = mem_birth;
+	}
+
+	public String getMem_nick() {
+		return mem_nick;
+	}
+
+	public void setMem_nick(String mem_nick) {
+		this.mem_nick = mem_nick;
+	}
+
+	public String getMem_email() {
+		return mem_email;
+	}
+
+	public void setMem_email(String mem_email) {
+		this.mem_email = mem_email;
+	}
+
+	
+	//toString
+	@Override
+	public String toString() {
+		return "MemberVo [mem_num=" + mem_num + ", mem_level=" + mem_level + ", mem_name=" + mem_name + ", mem_id="
+				+ mem_id + ", mem_pwd=" + mem_pwd + ", mem_gender=" + mem_gender + ", mem_addr=" + mem_addr
+				+ ", mem_tel=" + mem_tel + ", mem_birth=" + mem_birth + ", mem_nick=" + mem_nick + ", mem_email="
+				+ mem_email + "]";
 	}
 	
-public int IdCheck(String id) {
-		
-		ResultSet      rs         = null;
-		
-		try {
-			DBConn    db   =  new DBConn();
-			conn           =  db.getConnection();
-			String    sql  =  " SELECT  MEM_num";
-					  sql += " FROM   MEM ";
-					  sql += " WHERE  MEM_num =  ? ";
-			pstmt          =  conn.prepareStatement(sql);
-			
-			pstmt.setString(1, id);
-			
-			rs       =  pstmt.executeQuery();
-			if( rs.next() ) {
-				return  0 ;
-				}else {
-				return  1 ;
-				}
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			try {
-				if(rs    != null )  rs.close();
-				if(pstmt != null )  pstmt.close();
-				//if(conn  != null )  conn.close();
-			} catch (SQLException e) {
-			}
-		}
-
-		return   -1;
-	}
+	
 }
