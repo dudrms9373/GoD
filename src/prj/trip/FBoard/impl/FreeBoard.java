51cdf1fe4edb277c6067f7bc0099651f622aa9c1
choi2864 <85035853+choi2864@users.noreply.github.com>
2021-12-06 20:07:33 +0900

    Add files via upload

---
+++ src/prj/trip/FBoard/impl/FreeBoard.java
@@ -0,0 +1,61 @@
+package FBoard.impl;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import FBoard.Vo.FreeBoardVo;
+import FBoard.Vo.PagingVo;
+import TBoard.service.Action;
+import memebr.dao.MemberDao;
+
+public class FreeBoard implements Action {
+
+	@Override
+	public void excute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		int end;
+		int start;
+		int number;
+		String num = request.getParameter("pagenum");
+		System.out.println("숫자"+num);
+		
+		MemberDao dao = new MemberDao();
+		
+		//페이징
+		PagingVo pvo = new PagingVo();
+		pvo.setPageSize(4);
+		int s = pvo.getPageSize();
+		if(num == null) {
+			pvo.setPageNo(1);
+			end = s * 1;
+			start = end - s +1;
+			
+		}else {
+			number = Integer.parseInt(num);
+			pvo.setPageNo(number);
+			System.out.println("숫자 변화값"+number);
+			end = s * number;
+			start = end - s +1;
+			
+		}
+		pvo.setTotalCount(dao.getCount());
+		//pvo.setStartPageNo(0*s+1);
+		//pvo.setEndPageNo(2*);
+		request.setAttribute("pvo", pvo);
+		
+		//게시판 목록
+		System.out.println(start); //게시물 시작번호
+		System.out.println(end); //게시물 끝번호
+		System.out.println(pvo.getPageSize()); //한페이지에 보이는 게시물 갯수
+		List<FreeBoardVo> fbvo =   dao.getFreeBoardList(end , start);
+		request.setAttribute("fbvo", fbvo);
+		
+		String   path         =   "/fboard/FreeBoard.jsp";  
+		request.getRequestDispatcher(path).forward(request, response);
+		
+	}
+
+}
