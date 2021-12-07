5590c51d6a6acaf8b0c051c414a20366b19cfd08
choi2864 <85035853+choi2864@users.noreply.github.com>
2021-12-07 19:14:34 +0900

    Add files via upload

--- src/prj/trip/FBoard/dao/FreeBoardDao.java
+++ src/prj/trip/FBoard/dao/FreeBoardDao.java
@@ -1,4 +1,4 @@
-package prj.trip.FBoard.dao;
+package FBoard.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -7,12 +7,8 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import prj.trip.db.DBConn;
-import prj.trip.FBoard.Vo.FreeBoardVo;
-
-
-
-
+import FBoard.Vo.FreeBoardVo;
+import db.DBConn;
 
 public class FreeBoardDao {
 	
@@ -31,7 +27,7 @@ public List<FreeBoardVo> getFreeBoardList(int startNum, int endNum){
 	try {
 		DBConn db = new DBConn();
 		conn = db.getConnection();
-		 String sql =" SELECT AA.FB_NUM, AA.FB_TITLE, M.MEM_NICK , AA.FB_DATE, AA.FB_CNT , (SELECT COUNT(*) OVER   FROM FB_LIKE FBL,FREE_BOARD FBA  WHERE FBA.FB_NUM = FBL.FB_NUM )AS LIKECNT "
+		 String sql =" SELECT AA.FB_NUM, AA.FB_TITLE, M.MEM_NICK , AA.FB_DATE, AA.FB_CNT , NVL((SELECT COUNT(*) OVER   FROM FB_LIKE WHERE FB_NUM= ? ),0)AS LIKECNT "
 		 		+ " FROM ( SELECT ROWNUM AS RNUM, Z.* FROM ( SELECT * from FREE_BOARD A  order by A.FB_NUM asc ) Z WHERE ROWNUM <= ? ) AA , MEMBER M "
 		 		+ " WHERE RNUM >= ? "
 		 		+ " AND M.MEM_NUM = AA.MEM_NUM "
@@ -95,6 +91,164 @@ public int getCount(){
 	}
 	return count; // 총 레코드 수 리턴
 }
+//테이블에 저장할 회원번호를 가져오기
+public int getMemNum(String loginId) {
+	ResultSet rs=null;
+   int memNum = 0;
+   String sql = "SELECT MEM_NUM FROM MEMBER WHERE MEM_ID = ? ";
+   
+   try {
+      DBConn db         = new DBConn();
+      conn       = db.getConnection();
+      pstmt      = conn.prepareStatement(sql);
+      pstmt.setString(1, loginId);
+      rs = pstmt.executeQuery();
+      
+      if(rs.next()){
+         memNum = rs.getInt("MEM_NUM");
+      }
+   } catch (SQLException e) {
+      e.printStackTrace();
+   } finally {
+      try {
+    	  if(rs!=null)rs.close();
+    	  if(pstmt!=null)pstmt.close();
+    	  if(conn!=null)conn.close();
+   } catch (SQLException e) {
+      e.printStackTrace();
+   }
+   }
+         return memNum;
+}
+
+//게시물 클릭시 제목내용 불러오기
+public FreeBoardVo getFBoard(String fbnum) {
+	ResultSet rs=null;
+	FreeBoardVo fbv = new FreeBoardVo();
+	String sql = "SELECT FB_NUM, FB_TITLE, FB_CONT, FB_CNT, FB_DATE,(SELECT MM.MEM_NICK FROM FREE_BOARD FBB , MEMBER MM WHERE FBB.FB_NUM = ? AND MM.MEM_NUM = FBB.MEM_NUM)AS NICK , "
+			+ "NVL((SELECT COUNT(*) OVER   FROM FB_LIKE WHERE FB_NUM= ? ),0)AS LIKECNT, "
+			+ "(SELECT MEM_NICK FROM MEMBER M , FREE_BOARD FBC WHERE  M.MEM_NUM = FBC.MEM_NUM AND FBC.fb_num = ?)AS NICK "
+			+ "FROM FREE_BOARD WHERE FB_NUM = ? ";
+ 
+ try {
+    DBConn db         = new DBConn();
+    conn       = db.getConnection();
+    pstmt      = conn.prepareStatement(sql);
+    pstmt.setString(1, fbnum);
+    pstmt.setString(2, fbnum);
+    pstmt.setString(3, fbnum);
+    pstmt.setString(4, fbnum);
+    rs = pstmt.executeQuery();
+    
+    if(rs.next()){
+       String fbn = rs.getString("FB_NUM");
+       String title = rs.getString("FB_TITLE");
+       String cont = rs.getString("FB_CONT");
+       String cnt = rs.getString("FB_CNT");
+       String date = rs.getString("FB_DATE");
+       String like = rs.getString("LIKECNT");
+       String nick = rs.getString("NICK");
+       
+       fbv = new FreeBoardVo(fbn, title, date, cnt, cont, like, nick);
+    }
+ } catch (SQLException e) {
+    e.printStackTrace();
+ } finally {
+    try {
+  	  if(rs!=null)rs.close();
+  	  if(pstmt!=null)pstmt.close();
+  	  if(conn!=null)conn.close();
+ } catch (SQLException e) {
+    e.printStackTrace();
+ }
+ }
+       return fbv;
+}
+
+public String getNick(String id) {  //닉네임 가져오기
+	
+	ResultSet      rs         = null;
+	
+	try {
+		DBConn    db   =  new DBConn();
+		conn           =  db.getConnection();
+		String    sql  =  " SELECT  MEM_NICK";
+		sql += " FROM   MEMBER ";
+		sql += " WHERE  MEM_id =  ? ";
+		pstmt          =  conn.prepareStatement(sql);
+		
+		pstmt.setString(1, id);
+		
+		rs       =  pstmt.executeQuery();
+		if( rs.next() ) {
+			return rs.getString("MEM_NICK");
+		}
+	} catch (SQLException e) {
+		e.printStackTrace();
+	} finally {
+		try {
+			if(rs    != null )  rs.close();
+			if(pstmt != null )  pstmt.close();
+			if(conn  != null )  conn.close();
+		} catch (SQLException e) {
+		}
+	}
+	
+	return   "아이디없음";
+}
+
+public void CntUpdate(String fbnum) {
+	ResultSet rs =null;
+	DBConn db = new DBConn();
+	conn = db.getConnection();
+	String sql=" UPDATE FREE_BOARD SET FB_CNT = FB_CNT+1 WHERE FB_NUM = ? ";
+	try {
+		pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, fbnum);
+		
+		int r = pstmt.executeUpdate();
+		conn.commit();
+		
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}finally {
+	    try {
+	    	  if(pstmt!=null)pstmt.close();
+	    	  if(conn!=null)conn.close();
+	   } catch (SQLException e) {
+	      e.printStackTrace();
+	   }
+	   }
+}
+
+public int InsertFBboard(String title, String cont, int memnum) {
+	ResultSet rs = null;
+	String sql ="INSERT INTO free_board ( fb_num, fb_title, fb_cont, fb_cnt, mem_num)"
+			+ " VALUES ( (SELECT NVL( MAX(fb_num),0 )+1 FROM free_board), ?, ? , 0, ? )";
+			
+	DBConn db = new DBConn();
+	conn =  db.getConnection();
+	try {
+		pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, title);
+		pstmt.setString(2, cont);
+		pstmt.setInt(3, memnum);
+		
+		rs = pstmt.executeQuery();
+		
+		if(rs.next()) {
+			conn.commit();
+			return 1;
+		}
+	} catch (SQLException e) {
+		e.printStackTrace();
+	}
+	
+	
+	return 0;
+	
+}
+
 
 
 
