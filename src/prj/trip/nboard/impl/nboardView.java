150934325a60d5cbd527c33fcc1d6ab116c24f72
zerogeun <55429152+dudrms9373@users.noreply.github.com>
2021-12-10 17:13:56 +0900

    Update nboardView.java

--- src/prj/trip/nboard/impl/nboardView.java
+++ src/prj/trip/nboard/impl/nboardView.java
@@ -15,16 +15,20 @@ public class nboardView implements Action {
 
 	@Override
 	public void excute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+		request.setCharacterEncoding("UTF-8");
 		HttpSession session = request.getSession();
 		
 		int num = Integer.parseInt(request.getParameter("nb_num"));
+		String nb_nick = request.getParameter("nb_nick");
+		String nick = (String) session.getAttribute("LoginNick");
 		
 		NBoardDao dao = new NBoardDao();
 
 		NBoardVo vo = dao.getnBoard(num);
 		
 		request.setAttribute("nboardView", vo);
+		request.setAttribute("nick", nick);
+		request.setAttribute("nb_nick", nb_nick);
 		
 		String path = "/view/nboard/nboardView.jsp";
 
