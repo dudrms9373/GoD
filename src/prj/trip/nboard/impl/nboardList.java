64523c31a466c16a4336cd2c2e7106fd614c0e55
zerogeun <55429152+dudrms9373@users.noreply.github.com>
2021-12-10 17:16:03 +0900

    Update nboardList.java

--- src/prj/trip/nboard/impl/nboardList.java
+++ src/prj/trip/nboard/impl/nboardList.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import prj.trip.member.dao.MemberDao;
 import prj.trip.nboard.dao.NBoardDao;
 import prj.trip.nboard.service.Action;
 import prj.trip.nboard.vo.NBoardVo;
@@ -16,11 +18,30 @@ public class nboardList implements Action {
 
 	@Override
 	public void excute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-
-		NBoardDao dao = new NBoardDao();
+		request.setCharacterEncoding("UTF-8");
+		HttpSession session = request.getSession();
 		
-		List<NBoardVo> nboardList = dao.getnBoardList();
+		
+		String id = (String) session.getAttribute("LoginId");
+		
+		int currentpage = 1; //현재페이지
+		int dpp = 10; // 한페이지당 자료 수
+		
+		NBoardDao dao = new NBoardDao();
+		MemberDao mdao = new MemberDao();
+		
+		int level = mdao.getMemLevel(id);
+
+		
+		int totalData = dao.getDataCount();
+		
+		request.setAttribute("dpp", dpp);
+		request.setAttribute("totalData", totalData);
+		request.setAttribute("id", id);
+		request.setAttribute("level", level);
+		
+		
+		List<NBoardVo> nboardList = dao.getPagingData(currentpage,dpp-10);
 
 		request.setAttribute("nboardList", nboardList);
 
