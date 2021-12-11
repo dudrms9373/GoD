6233de36d03b0950f4392f08b06690897b12015e
seunggod <92019271+seunggod@users.noreply.github.com>
2021-12-11 21:18:09 +0900

    Update TBoardWriteAction.java

--- src/prj/trip/tboard/service/TBoardWriteAction.java
+++ src/prj/trip/tboard/service/TBoardWriteAction.java
@@ -54,12 +54,12 @@ public class TBoardWriteAction implements Action {
 	    	bcont3 = multiRequest.getParameter("bcont3");
 	    	bcont4 = multiRequest.getParameter("bcont4");
 	    	bcontbox = bcont1;
-	    	if( bcont2!=null||!bcont2.trim().equals(""))
-	    		bcontbox+="%111%"+bcont2;
-	    	if( bcont3!=null||!bcont3.trim().equals(""))
-	    		bcontbox+="%111%"+bcont3;
-	    	if( bcont4!=null||!bcont4.trim().equals(""))
-	    		bcontbox+="%111%"+bcont4;
+	    	
+	    	bcontbox+="%111%"+bcont2;
+	    	
+	    	bcontbox+="%111%"+bcont3;
+	    	
+	    	bcontbox+="%111%"+bcont4;
 	    	
 	    	Enumeration files = multiRequest.getFileNames();
 	    	
