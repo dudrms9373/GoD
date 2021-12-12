4e103e393469bed80ddede818325466baa6a12b6
seunggod <92019271+seunggod@users.noreply.github.com>
2021-12-12 14:04:17 +0900

    관리자 여부 확인코드 추가

--- src/prj/trip/tboard/service/TBoardWriteForm.java
+++ src/prj/trip/tboard/service/TBoardWriteForm.java
@@ -13,6 +13,10 @@ public class TBoardWriteForm implements Action {
 	public void excute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
 		String      loginId = (String) session.getAttribute("LoginId");
+		//관리자 권한 확인
+		int uLevel = getMemLevel(loginId);
+		request.setAttribute("uLevel",uLevel);
+		//관리자여부를  확인하지 못하게 하기 위해 구분할만한 표시는 주지않는다.
 		request.setAttribute("nickname", loginId);
 		
 		String path="/view/tboard/tbwrite.jsp";
