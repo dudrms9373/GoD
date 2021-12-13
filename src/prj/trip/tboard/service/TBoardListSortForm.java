01d0054364976de54ba6ac54994284781c06aacc
seunggod <92019271+seunggod@users.noreply.github.com>
2021-12-13 13:39:04 +0900

    Update TBoardListSortForm.java

--- src/prj/trip/tboard/service/TBoardListSortForm.java
+++ src/prj/trip/tboard/service/TBoardListSortForm.java
@@ -6,7 +6,9 @@ import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
+import prj.trip.member.dao.MemberDao;
 import prj.trip.tboard.dao.TBoardDao;
 import prj.trip.tboard.vo.TBoardVo;
 
@@ -17,10 +19,19 @@ public class TBoardListSortForm implements Action {
 		request.setCharacterEncoding("UTF-8");
 		int currentPage = 1; // 첫페이지를 보여줌
 		int dpp         = 0; // 기본 페이지당 자료수는 10
+		HttpSession session = request.getSession();
+		
+		String    loginId   = (String) session.getAttribute("LoginId");
+		MemberDao mDao = new MemberDao();
+		//관리자 권한 확인
+		int uLevel = mDao.getMemLevel(loginId);
+		request.setAttribute("uLevel",uLevel);
+		//관리자여부를  확인하지 못하게 하기 위해 구분할만한 표시는 주지않는다.
 		String sodNum   = request.getParameter("odNum");
 		int    odNum    = Integer.parseInt(sodNum);
 		request.setAttribute("odNum", odNum);
-		
+		int searchNum   = 0;
+		request.setAttribute("searchNum", searchNum);
 		TBoardDao dao = new TBoardDao();
 		int totalData   = dao.getDataCount();
 		request.setAttribute("dpp", dpp+10);
